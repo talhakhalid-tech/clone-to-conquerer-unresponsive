9a170263107c1490a6c6cb913fbe7bd5f0b6c85c
talhakhalid-tech <talhakhalid0323@gmail.com>
2020-06-04 12:20:23 +0500

    third commit

--- index.js
+++ index.js
@@ -31,10 +31,6 @@ const homeHeight = document.getElementById('home').scrollHeight
 const aboutHeight = document.getElementById('about-us').scrollHeight
 const servicesHeight = document.getElementById('services').scrollHeight
 const contactHeight = document.getElementById('contact-us').scrollHeight
-const homeHead1 =  document.getElementById('home-head1')
-const homeHead2 = document.getElementById('home-head2')
-const parallaxHead = document.getElementById('parallax-head')
-const parallaxText = document.getElementById('parallax-text')
 
 window.addEventListener('scroll',()=>{
         if(homeHeight>window.scrollY){
@@ -42,47 +38,23 @@ window.addEventListener('scroll',()=>{
             about.removeAttribute('class')
             services.removeAttribute('class')
             contact.removeAttribute('class')
-            homeHead1.setAttribute('class','home-animation')
-            homeHead2.setAttribute('class','home-animation')
-            parallaxHead.removeAttribute('class')
-            parallaxText.removeAttribute('class')
-            parallaxHead.style.visibility = 'hidden'
-            parallaxText.style.visibility = 'hidden'
         }
         else if(homeHeight+aboutHeight>window.scrollY){
             about.setAttribute("class", "active");
             home.removeAttribute('class')
             services.removeAttribute('class')
             contact.removeAttribute('class')
-            parallaxHead.removeAttribute('class')
-            parallaxText.removeAttribute('class')
-            homeHead1.removeAttribute('class')
-            homeHead2.removeAttribute('class')
-            parallaxHead.style.visibility = 'hidden'
-            parallaxText.style.visibility = 'hidden'
         }
         else if(homeHeight+aboutHeight+servicesHeight>window.scrollY){
             services.setAttribute("class", "active");
             home.removeAttribute('class')
             about.removeAttribute('class')
             contact.removeAttribute('class')
-            homeHead1.removeAttribute('class')
-            homeHead2.removeAttribute('class')
-            parallaxHead.style.visibility = 'visible'
-            parallaxText.style.visibility = 'visible'
-            parallaxHead.setAttribute('class','parallax-animation')
-            parallaxText.setAttribute('class','parallax-animation')
         }
         else if(homeHeight+aboutHeight+servicesHeight+contactHeight>window.scrollY){
             contact.setAttribute("class", "active");
             home.removeAttribute('class')
             about.removeAttribute('class')
             services.removeAttribute('class')
-            parallaxHead.removeAttribute('class')
-            parallaxText.removeAttribute('class')
-            homeHead1.removeAttribute('class')
-            homeHead2.removeAttribute('class')
-            parallaxHead.style.visibility = 'hidden'
-            parallaxText.style.visibility = 'hidden'
         }
 })
